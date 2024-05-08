6919b14b4db548f469d60e5614d38188bd980721
RavenGoku <raven.goku@gmail.com>
2024-05-09 00:35:37 +0100

    making a player class

---
+++ Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe_ConsoleGame
+{
+    internal class Player
+    {
+        public Player(string pName,string pSign)
+        {
+            PlayerName = pName;
+            PlayerSign = pSign;
+        }
+
+        ~Player()
+        {
+        }
+
+        //Properties
+        public string PlayerName { get; }
+
+        public string PlayerSign { get; }
+
+        //Functions
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,47 +1,166 @@
-﻿namespace TicTacToe_ConsoleGame
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace TicTacToe_ConsoleGame
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
-            SetField();
+            Player playerOne = new Player("Player One","O");
+            Player playerTwo = new Player("Player Two","X");
+
+            do
+            {
+                string[,] board = new string[3,3]
+                {
+                { "1","2","3" },
+                { "4","5","6" },
+                { "7","8","9" }
+                };
+
+                SetField(board);
+
+                Console.ReadLine();
+                Console.Clear();
+            } while( true );
         }
 
-        public static void SetField()
+        private static void SetField(string[,] board)
         {
+            Console.Clear();
             Console.WriteLine("     |     |     ");
-            Console.WriteLine("  {0}  |  {1}  |  {2}   ",1,2,3);
+            Console.WriteLine("  {0}  |  {1}  |  {2}   ",board[0,0],board[0,1],board[0,2]);
             Console.WriteLine("_____|_____|_____");
             Console.WriteLine("     |     |     ");
-            Console.WriteLine("  {0}  |  {1}  |  {2}   ",1,2,3);
+            Console.WriteLine("  {0}  |  {1}  |  {2}   ",board[1,0],board[1,1],board[1,2]);
             Console.WriteLine("_____|_____|_____");
             Console.WriteLine("     |     |     ");
-            Console.WriteLine("  {0}  |  {1}  |  {2}   ",1,2,3);
+            Console.WriteLine("  {0}  |  {1}  |  {2}   ",board[2,0],board[2,1],board[2,2]);
             Console.WriteLine("     |     |     ");
         }
 
-        private bool WinChecker(string[,] board)
+        private static bool PlayerInput(Player player,string[,] board)
+        {
+            string input;
+            bool isValidInput = false;
+            do
+            {
+                Console.Write(player.PlayerName + ": ");
+                input = Console.ReadLine();
+                switch( input )
+                {
+                    case "1":
+                        if( board[0,0] != "X" && board[0,0] != "O" )
+                        {
+                            board[0,0] = player.PlayerSign;
+                            isValidInput = true;
+                        }
+                        else
+                            Console.WriteLine("Already filled, Try Again! ");
+                        break;
+
+                    case "2":
+                        if( board[0,1] != "X" && board[0,1] != "O" )
+                        {
+                            board[0,1] = player.PlayerSign;
+                            isValidInput = true;
+                        }
+                        else
+                            Console.WriteLine("Already filled, Try Again! ");
+                        break;
+
+                    case "3":
+                        if( board[0,2] != "X" && board[0,2] != "O" )
+                        {
+                            board[0,2] = player.PlayerSign;
+                            isValidInput = true;
+                        }
+                        else
+                            Console.WriteLine("Already filled, Try Again! ");
+                        break;
+
+                    case "4":
+                        if( board[1,0] != "X" && board[1,0] != "O" )
+                        {
+                            board[1,0] = player.PlayerSign;
+                            isValidInput = true;
+                        }
+                        else
+                            Console.WriteLine("Already filled, Try Again! ");
+                        break;
+
+                    case "5":
+                        if( board[1,1] != "X" && board[1,1] != "O" )
+                        {
+                            board[1,1] = player.PlayerSign;
+                            isValidInput = true;
+                        }
+                        else
+                            Console.WriteLine("Already filled, Try Again! ");
+                        break;
+
+                    case "6":
+                        if( board[1,2] != "X" && board[1,2] != "O" )
+                        {
+                            board[1,2] = player.PlayerSign;
+                            isValidInput = true;
+                        }
+                        else
+                            Console.WriteLine("Already filled, Try Again! ");
+                        break;
+
+                    case "7":
+                        if( board[2,0] != "X" && board[2,0] != "O" )
+                        {
+                            board[2,0] = player.PlayerSign;
+                            isValidInput = true;
+                        }
+                        else
+                            Console.WriteLine("Already filled, Try Again! ");
+                        break;
+
+                    case "8":
+                        if( board[2,1] != "X" && board[2,1] != "O" )
+                        {
+                            board[2,1] = player.PlayerSign;
+                            isValidInput = true;
+                        }
+                        else
+                            Console.WriteLine("Already filled, Try Again! ");
+                        break;
+
+                    case "9":
+                        if( board[2,2] != "X" && board[2,2] != "O" )
+                        {
+                            board[2,2] = player.PlayerSign;
+                            isValidInput = true;
+                        }
+                        else
+                            Console.WriteLine("Already filled, Try Again! ");
+                        break;
+                }
+            } while( !isValidInput );
+            return isValidInput;
+        }
+
+        private static bool WinChecker(string[,] board,Player player)
         {
             for( int row = 0, column = 0; row < board.GetLength(0); row++, column++ )
             {
-                //Horizontal and Vertical Check 'X'
-                if( board[row,0] == board[row,1] && board[row,1] == board[row,2] && board[row,column] == "X" ||
-                    board[0,row] == board[1,row] && board[1,row] == board[2,row] && board[row,column] == "X" )
+                //Horizontal and Vertical Check
+                if( board[row,0] == board[row,1] && board[row,1] == board[row,2] ||
+                    board[0,row] == board[1,row] && board[1,row] == board[2,row] )
+                {
+                    Console.WriteLine(player.PlayerName + " Won!");
                     return true;
-                //Diagonal and Reverse Diagonal Check 'X'
-                if( board[0,0] == board[1,1] && board[1,1] == board[2,2] && board[row,column] == "X" ||
-                    board[0,2] == board[1,1] && board[1,1] == board[2,0] && board[row,column] == "X" )
+                }
+                //Diagonal and Reverse Diagonal Check
+                if( board[0,0] == board[1,1] && board[1,1] == board[2,2] ||
+                    board[0,2] == board[1,1] && board[1,1] == board[2,0] )
+                {
+                    Console.WriteLine(player.PlayerName + " Won!");
                     return true;
-
-                //Horizontal and Vertical Check 'O'
-                if( board[row,0] == board[row,1] && board[row,1] == board[row,2] && board[row,column] == "O" ||
-                    board[0,row] == board[1,row] && board[1,row] == board[2,row] && board[row,column] == "O" )
-                    return true;
-
-                //Diagonal and Reverse Diagonal Check 'O'
-                if( board[0,0] == board[1,1] && board[1,1] == board[2,2] && board[row,column] == "O" ||
-                    board[0,2] == board[1,1] && board[1,1] == board[2,0] && board[row,column] == "O" )
-                    return true;
+                }
             }
 
             return false;
